cd7ca8599a0d2f554970a1e9b06936604232fad9
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-03-23 23:58:14 +0900

    Update conf.py
    # sphinxcontrib-seqdiag有効化

--- source/conf.py
+++ source/conf.py
@@ -60,3 +60,5 @@ html_theme = 'cloud'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+# sphinxcontrib-seqdiag有効化
+extensions = ['sphinxcontrib.seqdiag']
